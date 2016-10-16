4f3346ebca3d66d804492b694d2115d96dcfa318
Richard Styles <richard.styles@brunningandprice.co.uk>
2016-10-16 14:28:32 +0100

    Add config.json using nconf
    To use an encrypted config.json for production.

--- app/all.js
+++ app/all.js
@@ -1,26 +1,14 @@
-let childProcess = require('child_process');
-let pathToElectron = require('electron-prebuilt');
-
-shouldRebuildNativeModules(pathToElectron)
-  .then((shouldBuild) => {
-    if (!shouldBuild) return true;
-
-    let electronVersion = childProcess.execSync(`${pathToElectron} --version`, {
-      encoding: 'utf8',
-    });
-    electronVersion = electronVersion.match(/v(\d+\.\d+\.\d+)/)[1];
-
-    return installNodeHeaders(electronVersion)
-      .then(() => rebuildNativeModules(electronVersion, './node_modules'));
-  })
-  .catch((e) => {
-    console.error("Building modules didn't work!");
-    console.error(e);
-  });
 window.$ = window.jQuery = require('jquery');
 
 var ping = require('ping'),
 	keytar = require('keytar'),
-	SSH = require('simple-ssh');
+	SSH = require('simple-ssh'),
+	nconf = require('nconf');
+
+nconf.file({ file: 'config.json' });
 
 require('bootstrap-sass/assets/javascripts/bootstrap');
+
+ping.promise.probe(nconf.get('SSH_GATEWAY')).then(function (res) {
+	console.log(res);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -11,21 +11,21 @@ var rename = require('gulp-rename');
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('../js/*.js')
+    return gulp.src('js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass
 gulp.task('sass', function() {
-    return gulp.src('../scss/*.scss')
+    return gulp.src('scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('app/css'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('../js/*.js')
+    return gulp.src('js/*.js')
         .pipe(concat('all.js'))
         .pipe(gulp.dest('app'))
         .pipe(rename('all.min.js'))
@@ -35,8 +35,8 @@ gulp.task('scripts', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('../js/*.js', ['lint', 'scripts']);
-    gulp.watch('../scss/*.scss', ['sass']);
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+    gulp.watch('scss/*.scss', ['sass']);
 });
 
 // Default Task
